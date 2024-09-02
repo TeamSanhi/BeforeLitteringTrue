e15ff52a7b104e8014731aa9122a926890e9903b
GwonYongWon <bmp0176511@gmail.com>
2024-09-02 17:37:43 +0900

    projectFile(create)

---
+++ nanum/src/main/java/net/datasa/nanum/NanumApplication.java
@@ -0,0 +1,13 @@
+package net.datasa.nanum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanumApplication.class, args);
+	}
+
+}
---
+++ nanum/src/test/java/net/datasa/nanum/NanumApplicationTests.java
@@ -0,0 +1,13 @@
+package net.datasa.nanum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
