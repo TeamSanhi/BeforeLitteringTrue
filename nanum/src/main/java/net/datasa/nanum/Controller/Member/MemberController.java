ddcc6e489fd2340867ebbf173756835e97a892ef
lsh94109 <lsh94109@naver.com>
2024-09-03 17:18:12 +0900

    login.html GetMapping 작성
    로그인 화면 이동 GetMapping 작성

--- nanum/src/main/java/net/datasa/nanum/Controller/Member/MemberController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/Member/MemberController.java
@@ -1,5 +1,20 @@
 package net.datasa.nanum.Controller.Member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("member")
 public class MemberController {
+    /**
+     * 로그인 화면으로 이동
+     * @return login.html
+     */
+    @GetMapping("login")
+    public String login () {
+        return "memberView/login";
+    }
+
     
 }
