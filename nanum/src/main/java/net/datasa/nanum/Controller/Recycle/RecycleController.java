e72db3512e85e7650735c46a0ccd6d561d334beb
hyunn0o <oh0777235@gmail.com>
2024-09-04 17:52:23 +0900

    [버려요 페이지 url 맵핑] RecycleList html 및 Recycle 컨트롤러 수정

--- nanum/src/main/java/net/datasa/nanum/Controller/Recycle/RecycleController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/Recycle/RecycleController.java
@@ -1,5 +1,22 @@
 package net.datasa.nanum.Controller.Recycle;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("recycle")
+@RequiredArgsConstructor
 public class RecycleController {
-    
+    /**
+     * 사이트맵으로 이동하는 GetMapping
+     */
+    @GetMapping("recycleList")
+    public String recycleList() {
+        log.debug("recycleList로 이동");
+        return "recycleView/recycleList";
+    }
 }
