549eb36c201148a5fae28594a5ec955495664fc5
goldcow <hwangyk97@gmail.com>
2024-09-08 16:51:37 +0900

    [기능추가] 지도안의 영역 마커찍기

--- nanum/src/main/java/net/datasa/nanum/Controller/Share/ShareMapController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/Share/ShareMapController.java
@@ -1,28 +1,45 @@
 package net.datasa.nanum.Controller.Share;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.datasa.nanum.domain.dto.ShareBoardDTO;
+import net.datasa.nanum.service.ShareMapService;
 
 @Slf4j
 @Controller
 @RequestMapping("share")
+@RequiredArgsConstructor
 public class ShareMapController {
+
+    // ShareMapService 사용
+    private final ShareMapService shareMapService;
+
     /**
      * 남서 북서 위도경도를 받아와 속하는 게시판글들을 반환하는 컨트롤러
      */
     @ResponseBody
     @GetMapping("mapList")
-    public void mapList(
-        @RequestParam("swLat") double swLat,
-        @RequestParam("swLng") double swLng,
-        @RequestParam("neLat") double neLat,
-        @RequestParam("neLng") double neLng) {
-        //전달 받은 위도 경도 값 
+    public List<ShareBoardDTO> mapList(
+            @RequestParam("swLat") double swLat,
+            @RequestParam("swLng") double swLng,
+            @RequestParam("neLat") double neLat,
+            @RequestParam("neLng") double neLng) {
+        // 전달 받은 위도 경도 값
         log.debug("swLat:{}, swLng:{}, neLat:{}, neLng:{}", swLat, swLng, neLat, neLng);
+
+        // mapList함수를 이용해 해당 위치 값 안에 있는 게시글들을 가져온다.
+        List<ShareBoardDTO> shareBoardDTO = shareMapService.mapList(swLat, swLng, neLat, neLng);
+        // 올바르게 값을 전달받았는지 디버깅
+        log.debug("shareMapController에서 전달받은 값 : {}", shareBoardDTO);
+
+        return shareBoardDTO; // JSON 형태로 반환
     }
-}    
+}
--- nanum/src/main/java/net/datasa/nanum/repository/ShareBoardRepository.java
+++ nanum/src/main/java/net/datasa/nanum/repository/ShareBoardRepository.java
@@ -1,6 +1,10 @@
 package net.datasa.nanum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import net.datasa.nanum.domain.entity.ShareBoardEntity;
@@ -11,4 +15,11 @@ import net.datasa.nanum.domain.entity.ShareBoardEntity;
 @Repository
 public interface ShareBoardRepository extends JpaRepository<ShareBoardEntity, Integer> {
 
+    // 남서 북동 좌펴안의 게시판 글들을 부러오는 함수
+    @Query("SELECT e FROM ShareBoardEntity e WHERE e.shareLat > :swLat AND e.shareLat < :neLat AND e.shareLng > :swLng AND e.shareLng < :neLng")
+    List<ShareBoardEntity> findMapList(
+            @Param("swLat") double swLat,
+            @Param("swLng") double swLng,
+            @Param("neLat") double neLat,
+            @Param("neLng") double neLng);
 }
