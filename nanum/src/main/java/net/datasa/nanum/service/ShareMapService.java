b244538f0dda86d75268b0d6bbbe033b11fa3f3c
goldcow <hwangyk97@gmail.com>
2024-09-08 15:25:09 +0900

    [파일생성] ShareMapService.java 파일 생성

---
+++ nanum/src/main/java/net/datasa/nanum/service/ShareMapService.java
@@ -0,0 +1,18 @@
+package net.datasa.nanum.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 지도 관련 서비스
+ */
+@RequiredArgsConstructor
+@Transactional
+@Service
+@Slf4j
+public class ShareMapService {
+
+}
