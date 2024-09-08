fe6f21fcc72c1a3940f8de643e98c6a1b0d436b3
goldcow <hwangyk97@gmail.com>
2024-09-08 15:28:55 +0900

    [파일수정] 주석 붙임

--- nanum/src/main/java/net/datasa/nanum/security/WebSecurityConfig.java
+++ nanum/src/main/java/net/datasa/nanum/security/WebSecurityConfig.java
@@ -19,64 +19,59 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-    //로그인 없이 접근 가능한 경로
-    private static final String[] PUBLIC_URLS = {
-            "/"                         // root
-            , "/images/**"              // 이미지 경로
-            , "/css/**"                 // CSS파일
-            , "/js/**"                  // JavaSCript 파일
-            , "/member/join"            // 회원가입
-            , "/find/idFind"            // ID 찾기
-            , "/find/pwFind"            // 비밀번호 찾기
-            , "/find/idCheck"           // ID 중복 확인
-            , "/share/list"             // 나눔 리스트
-            , "/share/read"             // 나눔글 읽기
-//          , "/share/Save"             // 나눔글 작성
-            , "/share/download"         // 나눔 첨부파일 다운로드 
-            , "/share/mapList"          // 나눔 지도 리스트
-            , "/info/service"            // 서비스 소개
-            , "/info/siteMap"            // 사이트맵
-            , "/info/faq"                // FAQ
-            , "/recycle/list"            // 버려요 게시글 리스트
-            , "/recycle/read"            // 버려요 게시글 읽기
-    };
+        // 로그인 없이 접근 가능한 경로
+        private static final String[] PUBLIC_URLS = {
+                        "/" // root
+                        , "/images/**" // 이미지 경로
+                        , "/css/**" // CSS파일
+                        , "/js/**" // JavaSCript 파일
+                        , "/member/join" // 회원가입
+                        , "/find/idFind" // ID 찾기
+                        , "/find/pwFind" // 비밀번호 찾기
+                        , "/find/idCheck" // ID 중복 확인
+                        , "/share/list" // 나눔 리스트
+                        , "/share/read" // 나눔글 읽기
+                        , "/share/download" // 나눔 첨부파일 다운로드
+                        , "/share/mapList" // 나눔 지도 리스트
+                        , "/info/service" // 서비스 소개
+                        , "/info/siteMap" // 사이트맵
+                        , "/info/faq" // FAQ
+                        , "/recycle/list" // 버려요 게시글 리스트
+                        , "/recycle/read" // 버려요 게시글 읽기
+        };
 
-    // 로그인 실패 핸들러 의존성 주입
-    private final AuthenticationFailureHandler customFailureHandler;
+        // 로그인 실패 핸들러 의존성 주입
+        private final AuthenticationFailureHandler customFailureHandler;
 
-    @Bean
-    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(author -> author
-                        .requestMatchers(PUBLIC_URLS).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/member/loginForm")
-                        .usernameParameter("id")
-                        .passwordParameter("password")
-                        .loginProcessingUrl("/member/login")
-                        .defaultSuccessUrl("/", true)
-                        .failureHandler(customFailureHandler)   // 로그인 실패 핸들러
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/member/logout")
-                        .logoutSuccessUrl("/")
-                );
+        @Bean
+        protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(author -> author
+                                                .requestMatchers(PUBLIC_URLS).permitAll()
+                                                .anyRequest().authenticated())
+                                .httpBasic(Customizer.withDefaults())
+                                .formLogin(formLogin -> formLogin
+                                                .loginPage("/member/loginForm")
+                                                .usernameParameter("id")
+                                                .passwordParameter("password")
+                                                .loginProcessingUrl("/member/login")
+                                                .defaultSuccessUrl("/", true)
+                                                .failureHandler(customFailureHandler) // 로그인 실패 핸들러
+                                                .permitAll())
+                                .logout(logout -> logout
+                                                .logoutUrl("/member/logout")
+                                                .logoutSuccessUrl("/"));
 
-        http
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable);
-                
+                http
+                                .cors(AbstractHttpConfigurer::disable)
+                                .csrf(AbstractHttpConfigurer::disable);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    public BCryptPasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public BCryptPasswordEncoder getPasswordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
 }
