ef673c7256047c608e8b651bb103e120b8e0a19a
goldcow97 <hwangyk97@gmail.com>
2024-09-09 19:21:40 +0900

    [파일수정] ShareDTO에 String 타입으로 포멧하여 날짜를 저장할 수 있는 formatDate 추가

--- nanum/src/main/java/net/datasa/nanum/domain/dto/ShareBoardDTO.java
+++ nanum/src/main/java/net/datasa/nanum/domain/dto/ShareBoardDTO.java
@@ -16,20 +16,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ShareBoardDTO {
 
-    private Integer shareNum;       //게시글 번호
-    private Integer memberNum;      //회원 번호 
-    private Integer receiverNum;    //reciver 번호
-    private String shareTitle;      //제목
-    private String shareContents;   //내용
-    private Double shareLat;        //위도
-    private Double shareLng;        //경도
-    private Boolean shareCompleted; //완료 여부
-    private LocalDateTime shareDate;//게시글 등록일
-    private Integer reportCount;    //신고 횟수
-    private Integer bookmarkCount;  //북마크 갯수
-    private String imageFileName;   // 파일 이름 
+    private Integer shareNum; // 게시글 번호
+    private Integer memberNum; // 회원 번호
+    private Integer receiverNum; // reciver 번호
+    private String shareTitle; // 제목
+    private String shareContents; // 내용
+    private Double shareLat; // 위도
+    private Double shareLng; // 경도
+    private Boolean shareCompleted; // 완료 여부
+    private LocalDateTime shareDate;// 게시글 등록일
+    private Integer reportCount; // 신고 횟수
+    private Integer bookmarkCount; // 북마크 갯수
+    private String imageFileName; // 파일 이름
 
     // entity에 없는 변수명 추가
-    private String memberNickname;   //회원 이름 
-    private String memberId;         // 회원 ID
+    private String memberNickname; // 회원 이름
+    private String memberId; // 회원 ID
+    private String formatDate; // shareList sciprt에서 사용하기 위해 날짜
 }
--- nanum/src/main/java/net/datasa/nanum/service/ShareMapService.java
+++ nanum/src/main/java/net/datasa/nanum/service/ShareMapService.java
@@ -1,5 +1,7 @@
 package net.datasa.nanum.service;
 
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +25,8 @@ public class ShareMapService {
 
     // ShareBoardReposityory를 사용
     private final ShareBoardRepository shareBoardRepository;
+    // 날짜형식을 포멧하여 저장하기
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
     /**
      * 해당 위치값 안에 있는 게시물들을 불러오는 함수
@@ -41,13 +45,15 @@ public class ShareMapService {
         List<ShareBoardDTO> dtoList = new ArrayList<>();
         // entityList를 DTO로 변환해서 dtoList에 저장
         for (ShareBoardEntity entity : entityList) {
+
             ShareBoardDTO dto = ShareBoardDTO.builder()
-                    .memberNum(entity.getMember().getMemberNum()) // entity.getMember().getMemberNum() 사용
+                    .memberNum(entity.getMember().getMemberNum()) // 게시글을 작성한 사용자번호
+                    .shareNum(entity.getShareNum()) // 게시글 번호
                     .shareTitle(entity.getShareTitle()) // 제목
                     .shareContents(entity.getShareContents()) // 내용
                     .memberNickname(entity.getMember().getMemberNickname()) // share_board에 없는 컬럼 DTO 따로 만들어 Nickname저장
                     .shareDate(entity.getShareDate()) // 게시글 작성 날짜
-                    .shareNum(entity.getShareNum()) // 게시글 번호
+                    .formatDate(dateFormat.format(entity.getShareDate())) // String 타입으로 포멧된 데이터 형식
                     .imageFileName(entity.getImageFileName()) // 게시글 파일 이미지 이름
                     .shareLat(entity.getShareLat()) // 위도
                     .shareLng(entity.getShareLng()) // 경도
