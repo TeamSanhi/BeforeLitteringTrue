15025b3ca17c1bb4d11b2d202993b8745c6a9098
lsh94109 <lsh94109@naver.com>
2024-09-10 19:02:28 +0900

    이메일 인증 기능 구현중

---
+++ nanum/src/main/java/net/datasa/nanum/security/MailConfig.java
@@ -0,0 +1,40 @@
+package net.datasa.nanum.security;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+
+@Configuration
+public class MailConfig {
+
+	@Bean
+	public JavaMailSender mailSender() {
+		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+		javaMailSender.setHost("smtp.naver.com");
+		javaMailSender.setUsername("lsh94109");         // 네이버 아이디
+		javaMailSender.setPassword("password");   // 네이버 비밀번호
+		javaMailSender.setPort(465);   //엥 포트번호가 바뀐건가?
+        //javaMailSender.setPort(587);
+		
+		javaMailSender.setJavaMailProperties(getMailProperties());
+		javaMailSender.setDefaultEncoding("UTF-8");
+		
+		return javaMailSender;
+	}
+	
+	private Properties getMailProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("mail.transport.protocol", "smtp");       //프로토콜 설정
+		properties.setProperty("mail.smtp.auth", "true");                // smtp 인증
+		//properties.setProperty("mail.smtp.starttls.enable", "true");               // smtp strattles 사용
+		properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
+		properties.setProperty("mail.debug", "true");                    // 디버그 사용
+		properties.setProperty("mail.smtp.ssl.enable", "true");          // ssl 사용
+		return properties;
+	}
+
+}
---
+++ nanum/src/main/java/net/datasa/nanum/service/MailService.java
@@ -0,0 +1,120 @@
+package net.datasa.nanum.service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.datasa.nanum.domain.entity.MemberEntity;
+import net.datasa.nanum.repository.MemberRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    
+    private final JavaMailSender javaMailSender;
+    private final MemberRepository memberRepository;
+    
+    private String ePw;
+
+    private void checkDuplicatedEmail(String email){
+
+        MemberEntity memberEntity = memberRepository.findByMemberEmail(email);
+  
+        if (memberEntity == null) {
+            log.error("해당 이메일로 회원을 찾을 수 없습니다: {}", email);
+            throw new EntityNotFoundException("해당 이메일로 회원을 찾을 수 없습니다.");
+        }        
+    }
+
+    /**
+     * 메일 내용 작성
+     * @param to
+     * @return
+     * @throws MessagingException
+     * @throws UnsupportedEncodingException
+     */
+    public MimeMessage createMessage(String to) throws MessagingException, UnsupportedEncodingException {
+        
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        message.addRecipients(Message.RecipientType.TO, to);        // 보내는 대상
+		message.setSubject("회원가입 이메일 인증");  // 제목
+
+        String msgg ="";
+        msgg += "<div style='margin:100px;'>";
+		msgg += "<h1> 안녕하세요,</h1>";
+		msgg += "<h1> 에코나비입니다.</h1>";
+		msgg += "<br>";
+		msgg += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
+		msgg += "<br>";
+		msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+		msgg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+		msgg += "<div style='font-size:130%'>";
+		msgg += "CODE : <strong>";
+		msgg += ePw + "</strong><div><br/> "; // 메일에 인증번호 넣기
+		msgg += "</div>";
+		message.setText(msgg, "utf-8", "html");// 내용, charset 타입, subtype
+		// 보내는 사람의 이메일 주소, 보내는 사람 이름
+		message.setFrom(new InternetAddress("lsh94109@naver.com", "TeamSanhi"));// 보내는 사람
+
+		return message;
+    }
+
+    /**
+     * 랜덤 인증 코드 전송
+     * @return
+     */
+    public String createCode() {
+        Random random = new Random();
+        StringBuffer key = new StringBuffer();
+
+        for (int i = 0; i < 8; i++) { // 인증코드 6자리
+			int index = random.nextInt(3); // 0~2 까지 랜덤, random 값에 따라서 아래 switch 문이 실행됨
+
+			switch (index) {
+			case 0:
+				key.append((char) ((int) (random.nextInt(26)) + 97));
+				// a~z (ex. 1+97=98 => (char)98 = 'b')
+				break;
+			case 1:
+				key.append((char) ((int) (random.nextInt(26)) + 65));
+				// A~Z
+				break;
+			case 2:
+				key.append((random.nextInt(10)));
+				// 0~9
+				break;
+			}
+		}
+
+        return key.toString();
+    }
+
+    public String sendSimpleMessage(String to) throws Exception {
+
+        ePw = createCode();
+        MimeMessage message = createMessage(to); // 메일 발송
+        try {// 예외처리
+			javaMailSender.send(message);
+		} catch (MailException es) {
+            log.error("메일 발송 중 오류 발생: {}", es.getMessage());
+            throw new RuntimeException("메일 발송 중 오류가 발생했습니다.", es);
+		}
+
+
+		return ePw; // 메일로 보냈던 인증 코드를 서버로 반환
+
+
+    }
+}
