cd2cf1bc6d3b4ee09e6d5cff0ded021b86c7b561
goldcow97 <hwangyk97@gmail.com>
2024-09-11 12:48:32 +0900

    [파일생성] BookMark 파일생성

---
+++ nanum/src/main/java/net/datasa/nanum/domain/entity/BookMarkEntity.java
@@ -0,0 +1,40 @@
+package net.datasa.nanum.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "bookmark")
+public class BookMarkEntity {
+
+    // 북마크 번호
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmark_key")
+    private Integer bookmarkKey;
+
+    // 회원번호
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_num", referencedColumnName = "member_num")
+    private MemberEntity member;
+
+    // 게시글 번호
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "share_num", referencedColumnName = "share_num")
+    private ShareBoardEntity shareBoard;
+}
