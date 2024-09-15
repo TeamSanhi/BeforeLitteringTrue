b6ec9f8246f7d44da89ae1def20d723d8c149ce2
goldcow <hwangyk97@gmail.com>
2024-09-15 18:01:02 +0900

    [기능추가] 우리는... 완료된 게시물들 마커 찍히도록 수정

--- nanum/src/main/java/net/datasa/nanum/repository/ShareBoardRepository.java
+++ nanum/src/main/java/net/datasa/nanum/repository/ShareBoardRepository.java
@@ -27,16 +27,29 @@ public interface ShareBoardRepository extends JpaRepository<ShareBoardEntity, In
     // @Param("neLat") double neLat,
     // @Param("neLng") double neLng);
 
-    // 남서 북동 좌표안의 게시판 글들을 가져오는 함수 + 제목과 내용을 검색하는 기능 추가
-    @Query("SELECT e FROM ShareBoardEntity e " +
-            "WHERE e.shareLat > :swLat AND e.shareLat < :neLat " +
-            "AND e.shareLng > :swLng AND e.shareLng < :neLng " +
-            "AND (:search = '' OR e.shareTitle LIKE %:search% OR e.shareContents LIKE %:search%) " +
-            "ORDER BY e.shareNum DESC")
-    List<ShareBoardEntity> findMapList(
-            @Param("swLat") double swLat,
-            @Param("swLng") double swLng,
-            @Param("neLat") double neLat,
-            @Param("neLng") double neLng,
-            @Param("search") String search);
+        // 남서 북동 좌표안의 게시판 글들을 가져오는 함수 + 제목과 내용을 검색하는 기능 추가
+        @Query("SELECT e FROM ShareBoardEntity e " +
+                        "WHERE e.shareLat > :swLat AND e.shareLat < :neLat " +
+                        "AND e.shareLng > :swLng AND e.shareLng < :neLng " +
+                        "AND (:search = '' OR e.shareTitle LIKE %:search% OR e.shareContents LIKE %:search%) " +
+                        "ORDER BY e.shareNum DESC")
+        List<ShareBoardEntity> findMapList(
+                        @Param("swLat") double swLat,
+                        @Param("swLng") double swLng,
+                        @Param("neLat") double neLat,
+                        @Param("neLng") double neLng,
+                        @Param("search") String search);
+}
+
+        // 남서 북동 좌표안의 거래가 완료된 게시글들을 불러오는 쿼리문
+        @Query("SELECT e FROM ShareBoardEntity e " +
+                        "WHERE e.shareLat > :swLat AND e.shareLat < :neLat " +
+                        "AND e.shareLng > :swLng AND e.shareLng < :neLng " +
+                        "AND e.shareCompleted = true")
+        List<ShareBoardEntity> findServiceList(
+                        @Param("swLat") double swLat,
+                        @Param("swLng") double swLng,
+                        @Param("neLat") double neLat,
+                        @Param("neLng") double neLng);
+
 }
