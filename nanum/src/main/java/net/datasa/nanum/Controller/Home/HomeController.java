2e3eb2c060de5f2c7d33fe6a1e3166afbcd3d9fc
goldcow97 <hwangyk97@gmail.com>
2024-09-19 17:10:22 +0900

    [css합치기] shareRead css 합침

--- nanum/src/main/java/net/datasa/nanum/Controller/Home/HomeController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/Home/HomeController.java
@@ -7,20 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 홈 컨트롤러 
+ * 홈 컨트롤러
  */
 @Controller
 @Slf4j
 public class HomeController {
-    
+
     /**
      * 홈으로 이동하는 GetMapping
+     * 
      * @return
      */
-    @GetMapping({"","/"})
+    @GetMapping({ "", "/" })
     public String home() {
         log.debug("homeController를 지나간다");
         return "homeView/home";
     }
-    
+
 }
