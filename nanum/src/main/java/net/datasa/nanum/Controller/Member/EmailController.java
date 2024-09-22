d60a71be12dd46abf8a368089ad6737005fc2d33
goldcow <hwangyk97@gmail.com>
2024-09-22 18:47:07 +0900

    [파일추가] 이메일 전송을 위한 기초파일 정리

---
+++ nanum/src/main/java/net/datasa/nanum/Controller/Member/EmailController.java
@@ -0,0 +1,18 @@
+package net.datasa.nanum.Controller.Member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 이메일 사용을 위한 컨트롤러
+ */
+@Slf4j
+@Controller
+@RequestMapping("member")
+@RequiredArgsConstructor
+public class EmailController {
+
+}
