4c254dc4370fdd03f7354be4ae18c79af6c6b97f
goldcow97 <hwangyk97@gmail.com>
2024-09-24 08:33:59 +0900

    [기능추가] email 인증 미완료시 회원가입 불가능

--- nanum/src/main/java/net/datasa/nanum/Controller/Find/FindController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/Find/FindController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.datasa.nanum.repository.MemberRepository;
 import net.datasa.nanum.service.FindService;
-import net.datasa.nanum.service.MailService;
 
 @Slf4j
 @Controller
@@ -21,34 +19,7 @@ import net.datasa.nanum.service.MailService;
 @RequiredArgsConstructor
 public class FindController {
 
-    private final MemberRepository memberRepository;
     private final FindService findService;
-    private final MailService mailService;
-
-    // /**
-    //  * ID 중복 확인 창으로 이동
-    //  * @return  findView/idCheck.html
-    //  */
-    // @GetMapping("idCheck")
-    // public String idCheck (@RequestParam("memberId") String memberId,
-    //                        @RequestParam("memberNickname") String memberNickname,
-    //                        Model model) {
-
-    //     // id 중복 여부 확인                    
-    //     boolean idDuplicate = findService.idDuplicate(memberId);
-    //     // 닉네임 중복 여부 확인 
-    //     boolean nickDuplicate = findService.nickDuplicate(memberNickname);
-
-    //     log.debug("가져온 memberId값: {}", memberId);
-    //     log.debug("가져온 memberNickname값: {}", memberNickname);
-
-    //     model.addAttribute("idDuplicate", idDuplicate);
-    //     model.addAttribute("nickDuplicate", nickDuplicate);
-    //     model.addAttribute("memberId", memberId);
-    //     model.addAttribute("memberNickname", memberNickname);
-
-    //     return "findView/idCheck";
-    // }
 
     @GetMapping("idCheck")
     @ResponseBody
@@ -76,6 +47,7 @@ public class FindController {
 
     /**
      * 아이디 찾기 화면으로 이동
+     * 
      * @return findView/idFind.html
      */
     @GetMapping("idFind")
@@ -86,6 +58,7 @@ public class FindController {
 
     /**
      * 비밀번호 찾기 화면으로 이동
+     * 
      * @return findView/pwFind.html
      */
     @GetMapping("pwFind")
@@ -93,5 +66,5 @@ public class FindController {
         log.debug("pwFind.html로 이동");
         return "findView/pwFind";
     }
-    
+
 }
