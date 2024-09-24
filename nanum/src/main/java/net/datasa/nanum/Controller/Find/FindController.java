779f7d29a09068a3e266b996821fd254feacd716
goldcow97 <hwangyk97@gmail.com>
2024-09-24 16:33:14 +0900

    [기능 수정] 아이디와 닉네임 인증절차 구분하여 체크

--- nanum/src/main/java/net/datasa/nanum/Controller/Find/FindController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/Find/FindController.java
@@ -1,8 +1,5 @@
 package net.datasa.nanum.Controller.Find;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
