2ecdebb7c46721f4d5f9e4b1a476c84eb3ea55d9
goldcow <hwangyk97@gmail.com>
2024-09-29 16:36:51 +0900

    [파일 수정] 프로필 기본이미지 파일명 수정

--- nanum/src/main/java/net/datasa/nanum/domain/entity/MemberEntity.java
+++ nanum/src/main/java/net/datasa/nanum/domain/entity/MemberEntity.java
@@ -52,7 +52,7 @@ public class MemberEntity {
 	private String memberNickname;
 	// 프로필 사진 이름
 	@Column(name = "member_file_name", length = 100)
-	private String memberFileName = "/images/profile-basic.png";
+	private String memberFileName = "profile-basic.png";
 	// 회원 가입일
 	@CreatedDate
 	@Column(name = "create_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
