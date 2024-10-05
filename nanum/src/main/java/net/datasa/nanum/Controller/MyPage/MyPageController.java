cfb16d4cba873ebf8f24d34ae2b716595553c955
goldcow <hwangyk97@gmail.com>
2024-10-05 14:47:20 +0900

    [css 추가 수정] myPage 기본정보 부분 수정

--- nanum/src/main/java/net/datasa/nanum/Controller/MyPage/MyPageController.java
+++ nanum/src/main/java/net/datasa/nanum/Controller/MyPage/MyPageController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.datasa.nanum.domain.dto.AlarmDTO;
 import net.datasa.nanum.domain.dto.MemberDTO;
 import net.datasa.nanum.domain.entity.MemberEntity;
+import net.datasa.nanum.repository.BookMarkRepository;
 import net.datasa.nanum.security.AuthenticatedUser;
 import net.datasa.nanum.service.AlarmService;
 import net.datasa.nanum.service.MemberService;
@@ -29,12 +30,15 @@ public class MyPageController {
     private final MemberService memberService;
 
     private final AlarmService alarmService;
+    // 북마크 테이블 접근
+    private final BookMarkRepository bookMarkRepository;
 
     @GetMapping("view")
     public String view(Model model, @AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
         Integer userNum = authenticatedUser.getNum();
         String userNickname = authenticatedUser.getNickname();
 
+        // 로그인한 사용자의 entity를 찾는다.
         MemberEntity member = memberService.getMemberByNum(userNum);
 
         Integer shareCount = shareService.getShareCount(member);
@@ -56,12 +60,19 @@ public class MyPageController {
 
         // 정렬
         alarmTotal.sort(Comparator.comparingInt(dayOrderMap::get));
+        // 알람 갯수
+        Integer alarmLength = alarmTotal.size();
+
+        // 북마크 갯수를 받아온다.
+        int bookmarkCount = bookMarkRepository.countByMember(member);
 
         model.addAttribute("userNickname", userNickname);
         model.addAttribute("shareCount", shareCount);
         model.addAttribute("alarmTotal", alarmTotal);
-        model.addAttribute("memberNum", member.getMemberNum());
-        model.addAttribute("pointCount", member.getMemberPoint());
+        model.addAttribute("alarmLength", alarmLength); // 알람 갯수
+        model.addAttribute("memberNum", member.getMemberNum()); // 회원번호
+        model.addAttribute("pointCount", member.getMemberPoint()); // 나눔 포인트
+        model.addAttribute("bookmarkCount", bookmarkCount); // 북마크 수
 
         log.debug("model : {}", model.toString());
 
--- nanum/src/main/java/net/datasa/nanum/repository/BookMarkRepository.java
+++ nanum/src/main/java/net/datasa/nanum/repository/BookMarkRepository.java
@@ -18,4 +18,6 @@ public interface BookMarkRepository extends JpaRepository<BookMarkEntity, Intege
     // memberNum과 shareNum을 사용해 북마크 존재 여부 확인
     Optional<BookMarkEntity> findByMemberAndShareBoard(MemberEntity member, ShareBoardEntity shareBoard);
 
+    // 특정 회원(member_num)의 북마크 개수를 셉니다.
+    Integer countByMember(MemberEntity member);
 }
